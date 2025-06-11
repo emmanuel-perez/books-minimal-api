985626ef424b6416382f40d47689cfaab29e00ff
emmanuel <emmanuelalba115@gmail.com>
2025-06-11 00:37:37 -0600

    Created BookModel and performed InitialMigration

---
+++ BooksMinimalApi/Data/BooksDbContext.cs
@@ -0,0 +1,24 @@
+﻿using BooksMinimalApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksMinimalApi.Data
+{
+    public class BooksDbContext: DbContext
+    {
+
+        private readonly IConfiguration _config;
+
+        public BooksDbContext(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_config.GetConnectionString("Default"));
+        }
+
+        public DbSet<BookModel> Books { get; set; } 
+        
+    }
+}
---
+++ BooksMinimalApi/Models/BookModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BooksMinimalApi.Models
+{
+    public class BookModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Author { get; set; } = null!;
+        public int ReleaseYear { get; set; }
+    }
+}
